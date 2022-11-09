97b9708783270457c228354f663c779c3f6b837a
oderevianko <oderevianko@gbsfo.com>
2022-11-09 19:46:33 +0200

    delete alert field when token was created/

--- dist/js/tool.js
+++ dist/js/tool.js
@@ -469,11 +469,7 @@ var render = function render() {
       d: "M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z",
       "clip-rule": "evenodd"
     }
-  })])]), _vm._v(" "), _c("div", {
-    staticClass: "flex items-center ml-3"
-  }, [_c("p", {
-    staticClass: "text-sm leading-5 text-yellow-700"
-  }, [_vm._v("\n                      " + _vm._s(_vm.__("Make sure to copy your new personal access token now. You won't be able to see it again!")) + "\n                    ")])])])])])])])])]), _vm._v(" "), _c("div", {
+  })])])])])])])])])]), _vm._v(" "), _c("div", {
     staticClass: "bg-30 px-6 py-3 flex"
   }, [_c("div", {
     staticClass: "flex items-center ml-auto"
