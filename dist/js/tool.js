3f022f5ba10f556578d24c195420c00c3226edc5
oderevianko <oderevianko@gbsfo.com>
2022-11-09 19:50:48 +0200

    delete alert field when token was created/

--- dist/js/tool.js
+++ dist/js/tool.js
@@ -449,27 +449,7 @@ var render = function render() {
     staticClass: "flex flex-col border-b border-40"
   }, [_c("div", {
     staticClass: "flex items-center py-6 px-8"
-  }, [_vm._t("default")], 2), _vm._v(" "), _c("div", {
-    staticClass: "flex items-center pb-6 px-8"
-  }, [_c("div", {
-    staticClass: "w-full rounded bg-danger-light text-danger-dark border-l-4 border-danger-dark p-4"
-  }, [_c("div", {
-    staticClass: "flex"
-  }, [_c("div", {
-    staticClass: "flex flex-shrink-0 items-center"
-  }, [_c("svg", {
-    staticClass: "h-5 w-5 text-yellow-400",
-    attrs: {
-      viewBox: "0 0 20 20",
-      fill: "currentColor"
-    }
-  }, [_c("path", {
-    attrs: {
-      "fill-rule": "evenodd",
-      d: "M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z",
-      "clip-rule": "evenodd"
-    }
-  })])])])])])])])])]), _vm._v(" "), _c("div", {
+  }, [_vm._t("default")], 2)])])])]), _vm._v(" "), _c("div", {
     staticClass: "bg-30 px-6 py-3 flex"
   }, [_c("div", {
     staticClass: "flex items-center ml-auto"
